722540fb74b59a6cc830a0678cc19096a02648da
Luis Garcia <lalejandro.1988@gmail.com>
2020-11-28 16:17:04 -0600

    Implementacion de estilos del AppRouter y LoginScreen

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+import { AuthRouter } from './AuthRouter';
+import { JournalScreen } from '../components/journal/JournalScreen';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div className="auth__main">
+                <div className="auth_box-container">
+                    <Switch>
+                        <Route
+                            path="/auth"
+                            component={ AuthRouter }
+                        />
+                        <Route
+                            exact
+                            path="/"
+                            component={ JournalScreen }
+                        />
+
+                        <Redirect to="/auth/login" />
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    )
+}
